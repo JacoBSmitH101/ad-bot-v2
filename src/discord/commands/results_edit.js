75bc0c70c29fc551d2f3c7452b6979dc947ad602
JacoBSmitH101 <jacobsmith2005@hotmail.com>
2025-12-21 17:23:44 +0000

    feat: add console logging for match and result in execute function of results_edit command

--- src/discord/commands/results_edit.js
+++ src/discord/commands/results_edit.js
@@ -54,6 +54,7 @@ export async function execute(interaction) {
                 legsB,
                 proofUrl: url ?? null,
             });
+        console.log(match, result);
 
         const embed = new EmbedBuilder()
             .setTitle("✏️ Result edited")
